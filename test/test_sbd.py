1ae179e72836f530062e8bb4ec55c4e549aed56a
Ulf Hamster <554c46@gmail.com>
2020-11-01 17:50:30 +0100

    typo corrected

--- test/test_sbd.py
+++ test/test_sbd.py
@@ -2,14 +2,14 @@ from nlptasks.sbd import (sbd_spacy_de, sbd_stanza_de)
 
 
 def test_01():
-    target = ["Die Kuh is bunt.", "Die Bäuerin mäht die Wiese."]
-    documents = ["Die Kuh is bunt. Die Bäuerin mäht die Wiese."]
+    target = ["Die Kuh ist bunt.", "Die Bäuerin mäht die Wiese."]
+    documents = ["Die Kuh ist bunt. Die Bäuerin mäht die Wiese."]
     sentences = sbd_spacy_de(documents)
     assert sentences == target
 
 
 def test_11():
-    target = ["Die Kuh is bunt.", "Die Bäuerin mäht die Wiese."]
-    documents = ["Die Kuh is bunt. Die Bäuerin mäht die Wiese."]
+    target = ["Die Kuh ist bunt.", "Die Bäuerin mäht die Wiese."]
+    documents = ["Die Kuh ist bunt. Die Bäuerin mäht die Wiese."]
     sentences = sbd_stanza_de(documents)
     assert sentences == target

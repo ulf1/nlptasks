3f25ff16f0d003f39044d464bb6113d70a6c39db
Ulf Hamster <554c46@gmail.com>
2020-11-01 17:38:13 +0100

    sbd algorithms added

---
+++ test/test_sbd.py
@@ -0,0 +1,15 @@
+from nlptasks.sbd import (sbd_spacy_de, sbd_stanza_de)
+
+
+def test_01():
+    target = ["Die Kuh is bunt.", "Die Bäuerin mäht die Wiese."]
+    documents = ["Die Kuh is bunt. Die Bäuerin mäht die Wiese."]
+    sentences = sbd_spacy_de(documents)
+    assert sentences == target
+
+
+def test_11():
+    target = ["Die Kuh is bunt.", "Die Bäuerin mäht die Wiese."]
+    documents = ["Die Kuh is bunt. Die Bäuerin mäht die Wiese."]
+    sentences = sbd_stanza_de(documents)
+    assert sentences == target

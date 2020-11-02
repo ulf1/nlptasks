7cc6a133ed2ec59b540fac737cf5dcdcf422d562
Ulf Hamster <554c46@gmail.com>
2020-11-02 18:18:03 +0100

    more unit tests added

--- test/test_token.py
+++ test/test_token.py
@@ -1,4 +1,5 @@
-from nlptasks.token import (token_spacy_de, token_stanza_de)
+from nlptasks.token import (
+    token_factory, token_spacy_de, token_stanza_de)
 
 
 def test_01():
@@ -11,9 +12,29 @@ def test_01():
     assert tokensequences == target
 
 
+def test_02():
+    target = [["Die", "Kuh", "ist", "bunt", "."],
+              ["Die", "Bäuerin", "mäht", "die", "Wiese", "."]]
+    sentences = ["Die Kuh ist bunt.", "Die Bäuerin mäht die Wiese."]
+    tokenizer_fn = token_factory("spacy")
+    assert tokenizer_fn.__name__ == "token_spacy_de"
+    sequences = tokenizer_fn(sentences)
+    assert sequences == target
+
+
 def test_11():
     target = [["Die", "Kuh", "ist", "bunt", "."],
               ["Die", "Bäuerin", "mäht", "die", "Wiese", "."]]
     sentences = ["Die Kuh ist bunt.", "Die Bäuerin mäht die Wiese."]
     tokensequences = token_stanza_de(sentences)
     assert tokensequences == target
+
+
+def test_22():
+    target = [["Die", "Kuh", "ist", "bunt", "."],
+              ["Die", "Bäuerin", "mäht", "die", "Wiese", "."]]
+    sentences = ["Die Kuh ist bunt.", "Die Bäuerin mäht die Wiese."]
+    tokenizer_fn = token_factory("stanza")
+    assert tokenizer_fn.__name__ == "token_stanza_de"
+    sequences = tokenizer_fn(sentences)
+    assert sequences == target

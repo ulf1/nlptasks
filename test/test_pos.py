1ef80bc0e7842f837a92e0d9e7106354c1798713
Ulf Hamster <554c46@gmail.com>
2020-11-03 18:43:22 +0100

    pos tagging added

---
+++ test/test_pos.py
@@ -0,0 +1,12 @@
+from nlptasks.pos import pos_factory
+
+
+def test_01():
+    targets = [[
+        "APPR", "ART", "NN", "ART", "NN", "VVFIN", "NE", "ART", "NN", "$."]]
+    seqs_token = [["Neben", "den", "Mitteln", "des", "Theaters", "benutzte",
+                   "Moran", "die", "Toncollage", "."]]
+    seqs_pos, TAGSET = pos_factory("spacy")(seqs_token)
+    # convert to targets to IDs
+    target_ids = [[TAGSET.index(pos) for pos in seq] for seq in targets]
+    assert seqs_pos == target_ids

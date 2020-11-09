087036cade4404463a3a3c6bfb55daea56ee1ec3
Ulf Hamster <554c46@gmail.com>
2020-11-09 18:53:42 +0100

    padding decorators with unit tests added

--- test/test_ner.py
+++ test/test_ner.py
@@ -10,3 +10,16 @@ def test_01():
     # convert to targets to IDs
     target_ids = [[SCHEME.index(ner) for ner in seq] for seq in targets]
     assert seqs_ner == target_ids
+
+
+def test_02():  # check pad_idseqs
+    targets = [[
+        "[PAD]", "[UNK]", "PER", "PER", "[UNK]",
+        "[UNK]", "[UNK]", "[UNK]", "[UNK]"]]
+    seqs_token = [[
+        "Der", "Helmut", "Kohl", "speist", "Schweinshaxe", "mit", "Kohl", "."]]
+    seqs_ner, SCHEME = ner_factory("spacy")(
+        seqs_token, maxlen=9, padding='pre', truncating='pre')
+    # convert to targets to IDs
+    target_ids = [[SCHEME.index(ner) for ner in seq] for seq in targets]
+    assert seqs_ner == target_ids

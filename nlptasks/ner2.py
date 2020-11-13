c49a874145185f1f51b64ca87b6754992c3ee082
UH <554c46@gmail.com>
2020-11-13 09:40:23 +0100

    move code to seperate module

---
+++ nlptasks/ner2.py
@@ -0,0 +1,70 @@
+from .padding import pad_maskseqs
+from typing import List
+import flair
+from .utils import FlairSentence
+
+
+def ner2_factory(name: str):
+    if name == "flair-multi":
+        return ner_flair_multi
+    else:
+        raise Exception(f"Unknown dependency parser: '{name}'") 
+
+
+@pad_maskseqs
+def ner_flair_multi(data: List[List[str]]) -> (List[List[str]], List[str]):
+    """flair 'multi-ner', returns sparse mask sequences of the 
+        CoNLL-03 NE scheme (4 tags) and BIONES chunks
+
+    Parameters:
+    -----------
+    data : List[List[str]]
+        List of token sequences
+
+    maxlen : Optional[int] = None
+        see @nlptasks.padding.pad_idseqs
+
+    padding : Optional[str] = 'pre'
+        see @nlptasks.padding.pad_idseqs
+
+    truncating : Optional[str] = 'pre'
+        see @nlptasks.padding.pad_idseqs
+
+    Returns:
+    --------
+    sequences : List[List[int]]
+        List of sequences that are sparse mask matrices. The rows indicate
+          the scheme.
+
+    seqlens : List[int]
+        Length of each sequence
+    
+    scheme : List[str]
+        4-class NER scheme (CoNLL-03) and BIONES chunks, 
+        ['PER', 'LOC', 'ORG', 'MISC', 'B', 'I', 'O', 'E', 'S']
+    
+    Example:
+    --------
+        maskseq, seqlen, SCHEME = ner_flair_multi2(tokens)
+    """
+    # (1) load flair model
+    tagger = flair.models.SequenceTagger.load('ner-multi')
+
+    # (2) Define the CoNLL-03 NER tagset as VOCAB
+    SCHEME = ['PER', 'LOC', 'ORG', 'MISC', 
+              'B', 'I', 'O', 'E', 'S']
+
+    # (3) NER recognize a pre-tokenized sentencens
+    maskseqs = []
+    seqlen = []
+    for sequence in data:
+        seq = FlairSentence(sequence)
+        tagger.predict(seq)
+        pairs = []
+        for i, t in enumerate(seq.tokens):
+            for key in t.get_tag("ner").value.split("-"):
+                pairs.append((SCHEME.index(key), i))        
+        maskseqs.append(pairs)
+    
+    # done
+    return maskseqs, seqlen, SCHEME
---
+++ test/test_ner2.py
@@ -0,0 +1,17 @@
+from nlptasks.ner2
+
+
+def test_11():
+    """ OhOh this fails
+    seqs_token = [[
+        "Der", "Helmut", "Kohl", "speist", "Schweinshaxe", "mit",
+        "Kohl", "in", "Berlin", "."]]
+    """
+    target_pairs = [
+        [(6, 0), (4, 1), (0, 1), (7, 2), (0, 2), (6, 3), (6, 4), (6, 5),
+         (6, 6), (6, 7), (8, 8), (1, 8), (6, 9)]]
+    seqs_token = [[
+        "Der", "Helmut", "Kohl", "speist", "Schweinshaxe", "mit",
+        "Blumenkohl", "in", "Berlin", "."]]
+    seqs_ner, seqlen, SCHEME = ner_factory("flair-multi2")(seqs_token)
+    assert seqs_ner == target_pairs

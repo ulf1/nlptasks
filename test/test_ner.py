4d3f79006a0cdfdf1a875d4597aa83aab1bc31e1
UH <554c46@gmail.com>
2020-11-13 11:30:45 +0100

    delete fn

--- test/test_ner.py
+++ test/test_ner.py
@@ -60,19 +60,3 @@ def test_12():  # check pad_idseqs
     # convert to targets to IDs
     target_ids = [[SCHEME.index(ner) for ner in seq] for seq in targets]
     assert seqs_ner == target_ids
-
-
-def test_21():
-    """ OhOh this fails
-    seqs_token = [[
-        "Der", "Helmut", "Kohl", "speist", "Schweinshaxe", "mit",
-        "Kohl", "in", "Berlin", "."]]
-    """
-    target_pairs = [
-        [(6, 0), (4, 1), (0, 1), (7, 2), (0, 2), (6, 3), (6, 4), (6, 5),
-         (6, 6), (6, 7), (8, 8), (1, 8), (6, 9)]]
-    seqs_token = [[
-        "Der", "Helmut", "Kohl", "speist", "Schweinshaxe", "mit",
-        "Blumenkohl", "in", "Berlin", "."]]
-    seqs_ner, seqlen, SCHEME = ner_factory("flair-multi2")(seqs_token)
-    assert seqs_ner == target_pairs

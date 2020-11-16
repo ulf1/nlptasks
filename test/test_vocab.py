f1ff3e4d88f686729c632fc879d8d50f43ca0ff8
UH <554c46@gmail.com>
2020-11-16 09:07:57 +0100

    fix #13

--- test/test_vocab.py
+++ test/test_vocab.py
@@ -10,3 +10,17 @@ def test1():
     assert "def" in VOCAB
     assert "ghi" not in VOCAB
     assert VOCAB == ["abc", "def"]
+
+
+def test2():
+    sequence = ["abc", "abc", "abc", "def", "def", "ghi"]
+    VOCAB = ["abc", "def"]
+    indicies = texttoken_to_index(sequence, VOCAB)
+    assert indicies == [0, 0, 0, 1, 1, 2]
+
+
+def test3():
+    sequence = ["abc", "abc", "abc", "def", "def", "ghi"]
+    VOCAB = ["abc", "def", "[UNK]"]
+    indicies = texttoken_to_index(sequence, VOCAB)
+    assert indicies == [0, 0, 0, 1, 1, 2]

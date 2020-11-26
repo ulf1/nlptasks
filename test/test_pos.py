7f07bb8d32ac89dd112def5c98fef29f1c39d7f9
UH <554c46@gmail.com>
2020-11-26 19:39:08 +0100

    two tests deleted

--- test/test_pos.py
+++ test/test_pos.py
@@ -73,17 +73,6 @@ def test_22():  # check pad_idseqs
     assert seqs_pos == target_ids
 
 
-def test_31():
-    targets = [[
-        "APPR", "ART", "NN", "ART", "NN", "VVFIN", "NE", "ART", "NN", "$."]]
-    seqs_token = [["Neben", "den", "Mitteln", "des", "Theaters", "benutzte",
-                   "Moran", "die", "Toncollage", "."]]
-    seqs_pos, TAGSET = pos_factory("someweta-de")(seqs_token)
-    # convert to targets to IDs
-    target_ids = [[TAGSET.index(pos) for pos in seq] for seq in targets]
-    assert seqs_pos == target_ids
-
-
 def test_32():  # check pad_idseqs
     targets = [[
         "[PAD]", "APPR", "ART", "NN", "ART", "NN",
@@ -92,17 +81,6 @@ def test_32():  # check pad_idseqs
                    "Moran", "die", "Toncollage", "."]]
     seqs_pos, TAGSET = pos_factory("someweta-de")(
         seqs_token, maxlen=11, padding='pre', truncating='pre')
-    # convert to targets to IDs
-    target_ids = [[TAGSET.index(pos) for pos in seq] for seq in targets]
-    assert seqs_pos == target_ids
-
-
-def test_41():
-    targets = [[
-        "APPR", "ART", "NN", "ART", "NN", "VVFIN", "NE", "ART", "NN", "$."]]
-    seqs_token = [["Neben", "den", "Mitteln", "des", "Theaters", "benutzte",
-                   "Moran", "die", "Toncollage", "."]]
-    seqs_pos, TAGSET = pos_factory("someweta-web-de")(seqs_token)
     # convert to targets to IDs
     target_ids = [[TAGSET.index(pos) for pos in seq] for seq in targets]
     assert seqs_pos == target_ids

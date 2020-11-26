fd5a31ef6a57a189058d1294988d03f0b7505af7
UH <554c46@gmail.com>
2020-11-26 13:01:16 +0100

    fix #86 add someweta

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(name='nlptasks',
           'flair==0.6.*',
           'nltk==3.5',
           'SoMaJo==2.1.1',
+          'SoMeWeTa==1.7.1',
           'pad-sequences>=0.5.*',
           'ray>=1.*'
       ],

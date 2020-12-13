e5a24f11e42f7ee30244e3324185e10fdd92bcb4
UH <554c46@gmail.com>
2020-12-14 00:37:56 +0100

    parse md to rst

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 
 from setuptools import setup
-import os
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+import m2r
 
 
 setup(name='nlptasks',
@@ -12,8 +8,8 @@ setup(name='nlptasks',
       description=(
           "Boilerplate code to wrap different libs for NLP tasks."
       ),
-      long_description=read('README.md'),
-      long_description_content_type='text/markdown',
+      long_description=m2r.parse_from_file('README.md'),
+      long_description_content_type='text/x-rst',
       url='http://github.com/ulf1/nlptasks',
       author='Ulf Hamster',
       author_email='554c46@gmail.com',
@@ -21,6 +17,7 @@ setup(name='nlptasks',
       packages=['nlptasks'],
       install_requires=[
           'setuptools>=40.0.0',
+          'm2r>=0.2.1',
           'torch>=1.1.0,<2',
           'tensorflow>=2.3.0,<3',
           'numpy>=1.18.0,<2',

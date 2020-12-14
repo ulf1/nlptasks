a7485340bdbe7937df7605f5a8124079eee481bc
UH <554c46@gmail.com>
2020-12-14 18:07:38 +0100

    fix #96

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
-
 from setuptools import setup
-import m2r
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(name='nlptasks',
@@ -8,8 +11,8 @@ setup(name='nlptasks',
       description=(
           "Boilerplate code to wrap different libs for NLP tasks."
       ),
-      long_description=m2r.parse_from_file('README.md'),
-      long_description_content_type='text/x-rst',
+      long_description=read('README.md'),
+      long_description_content_type='text/markdown',
       url='http://github.com/ulf1/nlptasks',
       author='Ulf Hamster',
       author_email='554c46@gmail.com',
@@ -17,7 +20,6 @@ setup(name='nlptasks',
       packages=['nlptasks'],
       install_requires=[
           'setuptools>=40.0.0',
-          'm2r>=0.2.1',
           'torch>=1.1.0,<2',
           'tensorflow>=2.3.0,<3',
           'numpy>=1.18.0,<2',

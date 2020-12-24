b40701796afb0208b09796eaa69ea157015bdd21
UH <554c46@gmail.com>
2020-12-24 16:24:41 +0100

    fix #37

--- nlptasks/testdata.py
+++ nlptasks/testdata.py
@@ -2,6 +2,12 @@ from typing import List
 
 
 def load_lpc_deu_news_2015_100K_sents() -> List[str]:
+    """Load LPC sentences corpus, German news, year 2015, 100k examples
+
+    Sources:
+    --------
+    - https://wortschatz.uni-leipzig.de/en/download/german
+    """
     try:
         fp = open("data/lpc/deu_news_2015_100K-sentences.txt", "r")
         X = [s.split("\t")[1].strip() for s in fp.readlines()]

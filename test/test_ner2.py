3cdca5e901411a84b3220477529294448bb1c408
UH <554c46@gmail.com>
2020-12-24 13:42:21 +0100

    comment with reason added

--- test/test_ner2.py
+++ test/test_ner2.py
@@ -21,6 +21,7 @@ def test_11():
 
 
 """
+# The test would require to much RAM in Github Actions
 def test_12():
     target_pairs = [
         [(6, 0), (4, 1), (0, 1), (7, 2), (0, 2), (6, 3), (6, 4), (6, 5),

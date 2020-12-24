9306a8912f907a075c70be3c4937f41c5c9bafa6
UH <554c46@gmail.com>
2020-12-24 08:10:48 +0100

    remove orphan dependency

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 
 setup(name='nlptasks',
-      version='0.2.8',
+      version='0.3.0',
       description=(
           "Boilerplate code to wrap different libs for NLP tasks."
       ),
@@ -32,8 +32,7 @@ setup(name='nlptasks',
           'nltk==3.5',
           'SoMaJo==2.1.1',
           'SoMeWeTa==1.7.1',
-          'pad-sequences>=0.5.*',
-          'ray>=1.*'
+          'pad-sequences>=0.5.*'
       ],
       scripts=[
           'scripts/nlptasks_downloader.py'

d1841cc1dd75331e3d11b4a27a6f821a166ef6f3
UH <554c46@gmail.com>
2020-12-24 12:22:20 +0100

    fix #101 pos

--- test/test_pos.py
+++ test/test_pos.py
@@ -1,4 +1,5 @@
 from nlptasks.pos import pos_factory
+import nlptasks as nt
 
 
 def test_01():
@@ -20,6 +21,22 @@ def test_02():  # check pad_idseqs
                    "Moran", "die", "Toncollage", "."]]
     seqs_pos, TAGSET = pos_factory("spacy")(
         seqs_token, maxlen=11, padding='pre', truncating='pre')
+    # convert to targets to IDs
+    target_ids = [[TAGSET.index(pos) for pos in seq] for seq in targets]
+    assert seqs_pos == target_ids
+
+
+def test_03():
+    targets = [[
+        "APPR", "ART", "NN", "ART", "NN", "VVFIN", "NE", "ART", "NN", "$."]]
+    seqs_token = [["Neben", "den", "Mitteln", "des", "Theaters", "benutzte",
+                   "Moran", "die", "Toncollage", "."]]
+
+    identifier = "spacy-de"
+    model = nt.pos.get_model(identifier)
+    fn = nt.pos.pos_factory(identifier)
+    seqs_pos, TAGSET = fn(seqs_token, model=model)
+
     # convert to targets to IDs
     target_ids = [[TAGSET.index(pos) for pos in seq] for seq in targets]
     assert seqs_pos == target_ids
@@ -49,6 +66,22 @@ def test_12():  # check pad_idseqs
     assert seqs_pos == target_ids
 
 
+def test_13():
+    targets = [[
+        "APPR", "ART", "NN", "ART", "NN", "VVFIN", "NE", "ART", "NN", "$."]]
+    seqs_token = [["Neben", "den", "Mitteln", "des", "Theaters", "benutzte",
+                   "Moran", "die", "Toncollage", "."]]
+
+    identifier = "stanza-de"
+    model = nt.pos.get_model(identifier)
+    fn = nt.pos.pos_factory(identifier)
+    seqs_pos, TAGSET = fn(seqs_token, model=model)
+
+    # convert to targets to IDs
+    target_ids = [[TAGSET.index(pos) for pos in seq] for seq in targets]
+    assert seqs_pos == target_ids
+
+
 def test_21():
     targets = [[
         "APPR", "ART", "NN", "ART", "NN", "VVFIN", "NE", "ART", "NN", "$."]]
@@ -72,3 +105,18 @@ def test_22():  # check pad_idseqs
     target_ids = [[TAGSET.index(pos) for pos in seq] for seq in targets]
     assert seqs_pos == target_ids
 
+
+def test_23():
+    targets = [[
+        "APPR", "ART", "NN", "ART", "NN", "VVFIN", "NE", "ART", "NN", "$."]]
+    seqs_token = [["Neben", "den", "Mitteln", "des", "Theaters", "benutzte",
+                   "Moran", "die", "Toncollage", "."]]
+
+    identifier = "flair-de"
+    model = nt.pos.get_model(identifier)
+    fn = nt.pos.pos_factory(identifier)
+    seqs_pos, TAGSET = fn(seqs_token, model=model)
+
+    # convert to targets to IDs
+    target_ids = [[TAGSET.index(pos) for pos in seq] for seq in targets]
+    assert seqs_pos == target_ids
--- test/test_pos2.py
+++ test/test_pos2.py
@@ -1,4 +1,5 @@
 from nlptasks.pos2 import pos2_factory, UPOS_TAGSET, UD2_FEATS
+import nlptasks as nt
 
 
 def test_11():
@@ -31,6 +32,27 @@ def test_12():  # check pad_maskseqs
 
     maskseqs, seqlen, SCHEME = pos2_factory("stanza-de")(
         seqs_token, maxlen=11, padding='pre', truncating='pre')
+
+    assert maskseqs == targets
+    assert seqlen == [10]
+    assert SCHEME == UPOS_TAGSET + UD2_FEATS
+
+
+def test_13():
+    targets = [
+        [(1, 0), (5, 1), (100, 1), (115, 1), (43, 1), (75, 1), (17, 1),
+        (7, 2), (100, 2), (43, 2), (75, 2), (5, 3), (103, 3), (115, 3),
+        (43, 3), (77, 3), (17, 3), (7, 4), (103, 4), (43, 4), (77, 4),
+        (15, 5), (135, 5), (77, 5), (171, 5), (145, 5), (124, 5), (11, 6),
+        (112, 6), (42, 6), (77, 6), (5, 7), (110, 7), (115, 7), (41, 7),
+        (77, 7), (17, 7), (7, 8), (110, 8), (41, 8), (77, 8), (12, 9)]]
+    seqs_token = [["Neben", "den", "Mitteln", "des", "Theaters", "benutzte",
+                   "Moran", "die", "Toncollage", "."]]
+
+    identifier = "stanza-de"
+    model = nt.pos2.get_model(identifier)
+    fn = nt.pos2.pos2_factory(identifier)
+    maskseqs, seqlen, SCHEME = fn(seqs_token, model=model)
 
     assert maskseqs == targets
     assert seqlen == [10]

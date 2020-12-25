2ed2593492f8033420e2a62befb2c6e97598e488
UH <554c46@gmail.com>
2020-12-25 17:55:22 +0100

    fix #26 unit tests

--- test/test_dephead.py
+++ test/test_dephead.py
@@ -46,3 +46,49 @@ def test_03():  # preload model
     assert seqlens == [8]
     for pair in target:
         assert pair in maskseqs[0]
+
+
+def test_11():
+    seqs_token = [[
+        "Der", "Helmut", "Kohl", "speist", "Schweinshaxe", "mit", "Kohl", "."]]
+    target = [
+        (46, 0), (46, 1), (47, 2), (47, 3), (47, 4), (47, 5), (49, 6), (47, 7),
+        (19, 0), (31, 1), (36, 2), (42, 3), (21, 4), (17, 5), (19, 6), (32, 7)]
+    
+    maskseqs, seqlens = nt.dephead.factory("stanza-de")(seqs_token)
+
+    assert seqlens == [8]
+    for pair in target:
+        assert pair in maskseqs[0]
+
+
+def test_12():  # check if calling pad_dephead causes an error
+    seqs_token = [[
+        "Der", "Helmut", "Kohl", "speist", "Schweinshaxe", "mit", "Kohl", "."]]
+    target = [
+        (46, 0), (46, 1), (47, 2), (47, 3), (47, 4), (47, 5), (49, 6), (47, 7),
+        (19, 0), (31, 1), (36, 2), (42, 3), (21, 4), (17, 5), (19, 6), (32, 7)]
+
+    maskseqs, seqlens = nt.dephead.factory("stanza-de")(
+        seqs_token, maxlen=9, padding='post', truncating='post')
+
+    assert seqlens == [8]
+    for pair in target:
+        assert pair in maskseqs[0]
+
+
+def test_13():  # preload model
+    seqs_token = [[
+        "Der", "Helmut", "Kohl", "speist", "Schweinshaxe", "mit", "Kohl", "."]]
+    target = [
+        (46, 0), (46, 1), (47, 2), (47, 3), (47, 4), (47, 5), (49, 6), (47, 7),
+        (19, 0), (31, 1), (36, 2), (42, 3), (21, 4), (17, 5), (19, 6), (32, 7)]
+
+    identifier = "stanza-de"
+    model = nt.dephead.get_model(identifier)
+    fn = nt.dephead.factory(identifier)
+    maskseqs, seqlens = fn(seqs_token, model=model)
+
+    assert seqlens == [8]
+    for pair in target:
+        assert pair in maskseqs[0]
